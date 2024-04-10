1715f7a5746736536a4c928b8bd6c2793cfe828f
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-04-10 15:49:45 +0545

    added react tabs in ExperienceComponent

--- src/components/Slider/ExperienceComponent.jsx
+++ src/components/Slider/ExperienceComponent.jsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { motion } from 'framer-motion'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css'
 
 export default function ExperienceComponent() {
   return (
@@ -17,39 +19,119 @@ export default function ExperienceComponent() {
             Add New Project
           </motion.button>
 
-          <div className='mt-12 flex'>
-            <nav className='my-2 flex justify-center'>
-              <p className='mx-2'>Project 1</p>
-              <p className='mx-2'>Project 2</p>
-              <p className='mx-2'>Project 3</p>
-              <p className='mx-2'>Project 4</p>
-            </nav>
-          </div>
+          <Tabs>
+            <TabList>
+              <Tab>Project 1</Tab>
+              <Tab>Project 2</Tab>
+              <Tab>Project 3</Tab>
+              <Tab>Project 4</Tab>
+            </TabList>
 
-          <div className='rounded-[20px] border border-[#B5B5B5] bg-[#D9D9D9]/20 p-4'>
-            <div className='flex justify-between gap-x-5'>
-              <div className='flex items-center'>
-                <img className=' rounded-t-lg object-cover' src='/image.png' alt='' />
-              </div>
-              <div>
-                <div className='flex items-center justify-between'>
-                  <h1 className='text-2xl font-bold'>Project Name</h1>
+            <TabPanel>
+              <div className='rounded-[20px] border border-[#B5B5B5] bg-[#D9D9D9]/20 p-4'>
+                <div className='flex justify-between gap-x-5'>
+                  <div className='flex items-center'>
+                    <img className=' rounded-t-lg object-cover' src='/image.png' alt='' />
+                  </div>
+                  <div>
+                    <div className='flex items-center justify-between'>
+                      <h1 className='text-2xl font-bold'>Project 1 Name</h1>
 
-                  <h1 className='text-xl font-semibold'>Educational</h1>
+                      <h1 className='text-xl font-semibold'>Educational</h1>
+                    </div>
+                    <p className='mt-4 '>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cum fugit corrupti aperiam saepe
+                      fugiat illum ad iure asperiores quod dolores inventore vitae, quasi nulla pariatur, molestiae
+                      animi itaque harum.
+                    </p>
+                    <p className='mt-4'>Skills : HTML, CSS, REACT.JS, DOTNET</p>
+                    <p className='mt-4'>Tools and Tech : VSCODE, FIGMA</p>
+                  </div>
                 </div>
-                <p className='mt-4 '>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cum fugit corrupti aperiam saepe fugiat
-                  illum ad iure asperiores quod dolores inventore vitae, quasi nulla pariatur, molestiae animi itaque
-                  harum.
-                </p>
-                <p className='mt-4'>Skills : HTML, CSS, NEXT.JS</p>
-                <p className='mt-4'>Tools and Tech : VSCODE, FIGMA</p>
+                <div className='flex justify-end'>
+                  <button className='rounded-xl bg-black p-2 hover:bg-white/20'>Read More</button>
+                </div>
               </div>
-            </div>
-            <div className='flex justify-end'>
-              <button className='rounded-xl bg-black p-2 hover:bg-white/20'>Read More</button>
-            </div>
-          </div>
+            </TabPanel>
+            <TabPanel>
+              <div className='rounded-[20px] border border-[#B5B5B5] bg-[#D9D9D9]/20 p-4'>
+                <div className='flex justify-between gap-x-5'>
+                  <div className='flex items-center'>
+                    <img className=' rounded-t-lg object-cover' src='/image.png' alt='' />
+                  </div>
+                  <div>
+                    <div className='flex items-center justify-between'>
+                      <h1 className='text-2xl font-bold'>Project 2 Name</h1>
+
+                      <h1 className='text-xl font-semibold'>Educational</h1>
+                    </div>
+                    <p className='mt-4 '>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cum fugit corrupti aperiam saepe
+                      fugiat illum ad iure asperiores quod dolores inventore vitae, quasi nulla pariatur, molestiae
+                      animi itaque harum.
+                    </p>
+                    <p className='mt-4'>Skills : HTML, CSS, NEXT.JS</p>
+                    <p className='mt-4'>Tools and Tech : VSCODE, FIGMA</p>
+                  </div>
+                </div>
+                <div className='flex justify-end'>
+                  <button className='rounded-xl bg-black p-2 hover:bg-white/20'>Read More</button>
+                </div>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className='rounded-[20px] border border-[#B5B5B5] bg-[#D9D9D9]/20 p-4'>
+                <div className='flex justify-between gap-x-5'>
+                  <div className='flex items-center'>
+                    <img className=' rounded-t-lg object-cover' src='/image.png' alt='' />
+                  </div>
+                  <div>
+                    <div className='flex items-center justify-between'>
+                      <h1 className='text-2xl font-bold'>Project 3 Name</h1>
+
+                      <h1 className='text-xl font-semibold'>Educational</h1>
+                    </div>
+                    <p className='mt-4 '>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cum fugit corrupti aperiam saepe
+                      fugiat illum ad iure asperiores quod dolores inventore vitae, quasi nulla pariatur, molestiae
+                      animi itaque harum.
+                    </p>
+                    <p className='mt-4'>Skills : NEXT.JS</p>
+                    <p className='mt-4'>Tools and Tech : VSCODE</p>
+                  </div>
+                </div>
+                <div className='flex justify-end'>
+                  <button className='rounded-xl bg-black p-2 hover:bg-white/20'>Read More</button>
+                </div>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className='rounded-[20px] border border-[#B5B5B5] bg-[#D9D9D9]/20 p-4'>
+                <div className='flex justify-between gap-x-5'>
+                  <div className='flex items-center'>
+                    <img className=' rounded-t-lg object-cover' src='/image.png' alt='' />
+                  </div>
+                  <div>
+                    <div className='flex items-center justify-between'>
+                      <h1 className='text-2xl font-bold'>Project 4 Name</h1>
+
+                      <h1 className='text-xl font-semibold'>Work</h1>
+                    </div>
+                    <p className='mt-4 '>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo cum fugit corrupti aperiam saepe
+                      fugiat illum ad iure asperiores quod dolores inventore vitae, quasi nulla pariatur, molestiae
+                      animi itaque harum.
+                    </p>
+                    <p className='mt-4'>Skills : HTML, CSS, NEXT.JS</p>
+                    <p className='mt-4'>Tools and Tech : VSCODE, FIGMA</p>
+                  </div>
+                </div>
+                <div className='flex justify-end'>
+                  <button className='rounded-xl bg-black p-2 hover:bg-white/20'>Read More</button>
+                </div>
+              </div>
+            </TabPanel>
+          </Tabs>
         </div>
       </div>
     </div>
