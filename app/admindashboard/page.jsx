77b8dfa1253fd9e64d05bbb625d8cf69a525de20
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-04-22 18:06:12 +0545

    made the sidebar in admindashboard change the content of the page

--- app/admindashboard/page.jsx
+++ app/admindashboard/page.jsx
@@ -8,106 +8,54 @@ import RadarChartComponent from '@/components/charts/RadarChartComparision'
 import DonutComponent from '@/components/charts/DonutChart'
 import SkillsRadarComponent from '@/components/charts/SkillsRadarChart'
 
-import { AnimatePresence, motion } from 'framer-motion'
-import { useState } from 'react'
-import { SiFramer, SiTailwindcss, SiReact, SiJavascript, SiCss3 } from 'react-icons/si'
 import { DndContext } from '@dnd-kit/core'
 
-const AdminDashboard = () => {
-  return (
-    <DndContext>
-      <div className='flex bg-slate-900/20 text-slate-100'>
-        <SideNav />
-        <div className='size-full'>
-          <div className='m-4 h-[35px] rounded'>Welcome, Admin</div>
-          <div className=' grid grid-flow-row grid-cols-2 gap-2 '>
-            <div className='m-4 flex size-fit justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
-              <div>
-                <h1>Line Chart</h1>
-                <p className='mt-5'>Some description about the chart</p>
-                <p className='mt-5 text-justify'>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit laborum hic rem voluptate minima, nam
-                  quasi sunt maiores delectus ducimus praesentium quam ea at fugiat perferendis provident! Nostrum,
-                  obcaecati nisi!
-                </p>
-              </div>
-              <LineComponent />
-            </div>
-            <div className='m-4 flex size-fit flex-col justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
-              <div>
-                <h1>Area Chart</h1>
-                <p className='mt-5'>Some description about the chart</p>
-                <p className='mt-5 text-justify'>Lorem ipsum dolor,</p>
-              </div>
-              <AreaChartComponent />
-            </div>
-            <div className='m-4 flex size-fit flex-col justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
-              <div>
-                <h1>Pie Chart</h1>
-                <p className='mt-5'>Some description about the chart</p>
-              </div>
-              <PieChartComponent />
-            </div>
-            <div className='m-4 flex size-fit flex-col justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
-              <div>
-                <h1>Radar Chart for comparision</h1>
-                <p className='mt-5'>Some description about the chart</p>
-              </div>
-              <RadarChartComponent />
-            </div>
-            <div className='m-4 flex size-fit flex-col justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
-              <div>
-                <h1>Radar Chart for comparision</h1>
-                <p className='mt-5'>Some description about the chart</p>
-              </div>
-              <DonutComponent />
-            </div>
-            <div className='m-4 flex size-fit flex-col justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
-              <div>
-                <h1>Radar Chart for comparision</h1>
-                <p className='mt-5'>Some description about the chart</p>
-              </div>
-              <SkillsRadarComponent />
-            </div>
-          </div>
-          <div className='m-4 flex size-fit flex-col justify-center rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
-            <div>Login Data Of Ram of a week</div>
-            <BubbleChartComponent />
-          </div>
-        </div>
-      </div>
-    </DndContext>
-  )
-}
+import { useState } from 'react'
 
-const SideNav = () => {
-  const [selected, setSelected] = useState(0)
+import { AnimatePresence, motion } from 'framer-motion'
+import { MdSpaceDashboard } from 'react-icons/md'
+import { CgProfile } from 'react-icons/cg'
+import { FaCalendarCheck } from 'react-icons/fa'
+import { IoIosSettings } from 'react-icons/io'
+import { FaRegChartBar } from 'react-icons/fa'
 
+//icons
+import { FaTasks } from 'react-icons/fa'
+import { FaRegClock } from 'react-icons/fa6'
+import { MdTask } from 'react-icons/md'
+
+function SideNav({ selected, setSelected }) {
   return (
-    <nav className='top-20 flex size-fit flex-col items-center gap-2 bg-slate-950 p-4'>
-      {/* Temp logo from https://logoipsum.com/ */}
-      <svg width='40' height='28' viewBox='0 0 40 28' fill='none' xmlns='http://www.w3.org/2000/svg' className='mb-4'>
-        <path
-          fillRule='evenodd'
-          clipRule='evenodd'
-          d='M9.98578 4.11462L0 14C1.99734 15.9773 4.27899 17.6437 6.76664 18.9474C7.45424 20.753 8.53203 22.4463 10 23.8995C15.5229 29.3668 24.4772 29.3668 30 23.8995C31.468 22.4463 32.5458 20.753 33.2334 18.9473C35.721 17.6437 38.0027 15.9773 40 14L30.0223 4.12266C30.0149 4.11527 30.0075 4.10788 30 4.1005C24.4772 -1.36683 15.5229 -1.36683 10 4.1005C9.99527 4.10521 9.99052 4.10991 9.98578 4.11462ZM29.0445 20.7309C26.1345 21.7031 23.0797 22.201 20 22.201C16.9203 22.201 13.8656 21.7031 10.9556 20.7309C11.2709 21.145 11.619 21.5424 12 21.9196C16.4183 26.2935 23.5817 26.2935 28 21.9196C28.381 21.5424 28.7292 21.145 29.0445 20.7309ZM12.2051 5.8824C12.9554 6.37311 13.7532 6.79302 14.588 7.13536C16.3038 7.83892 18.1428 8.20104 20 8.20104C21.8572 8.20104 23.6962 7.83892 25.412 7.13536C26.2468 6.79302 27.0446 6.3731 27.795 5.88238C23.4318 1.77253 16.5682 1.77254 12.2051 5.8824Z'
-          fill='#FFFFFF'
-        ></path>
-      </svg>
+    <nav className='top-20 flex size-fit w-[20%] flex-col gap-2 rounded-xl bg-slate-950/40 p-4'>
       <NavItem selected={selected === 0} id={0} setSelected={setSelected}>
-        <SiTailwindcss />
+        <div className='flex items-center justify-between'>
+          <MdSpaceDashboard />
+          <p>Dashboard</p>
+        </div>
       </NavItem>
       <NavItem selected={selected === 1} id={1} setSelected={setSelected}>
-        <SiReact />
+        <div className='flex items-center justify-between'>
+          <FaCalendarCheck />
+          <p>Calendar</p>
+        </div>
       </NavItem>
       <NavItem selected={selected === 2} id={2} setSelected={setSelected}>
-        <SiJavascript />
+        <div className='flex items-center justify-between'>
+          <CgProfile />
+          <p>Profile</p>
+        </div>
       </NavItem>
       <NavItem selected={selected === 3} id={3} setSelected={setSelected}>
-        <SiFramer />
+        <div className='flex items-center justify-between'>
+          <IoIosSettings />
+          <p>Settings</p>
+        </div>
       </NavItem>
       <NavItem selected={selected === 4} id={4} setSelected={setSelected}>
-        <SiCss3 />
+        <div className='flex items-center justify-between'>
+          <FaRegChartBar />
+          <p>Chart</p>
+        </div>
       </NavItem>
     </nav>
   )
@@ -133,6 +81,108 @@ const NavItem = ({ children, selected, id, setSelected }) => {
         )}
       </AnimatePresence>
     </motion.button>
+  )
+}
+
+const AdminDashboard = () => {
+  const [selected, setSelected] = useState(0)
+  return (
+    <DndContext>
+      <div className='flex bg-slate-900/20 text-slate-100'>
+        <SideNav selected={selected} setSelected={setSelected} />
+        {selected === 0 ? (
+          <div className='mx-4 size-full'>
+            <div className='my-4 h-[35px] rounded'>Welcome, Admin</div>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4'>
+              <div className='my-4 flex justify-between rounded-xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+                <div className='flex items-center gap-x-5'>
+                  <FaTasks />
+                  <h1>Total Projects</h1>
+                </div>
+
+                <p>---</p>
+              </div>
+              <div className='my-4 flex justify-between rounded-xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+                <div className='flex items-center gap-x-5'>
+                  <FaRegClock />
+                  <h1>Hours Logged</h1>
+                </div>
+                <p>---</p>
+              </div>
+              <div className='my-4 flex justify-between rounded-xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+                <div className='flex items-center gap-x-5'>
+                  <MdTask />
+                  <p>Total Tasks Completed</p>
+                </div>
+                <p>---</p>
+              </div>
+
+              <div className='my-4 flex justify-between rounded-xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+                <div className='flex items-center justify-between gap-x-5'>
+                  <MdTask />
+                  <p>Total Tasks Completed</p>
+                </div>
+                <p>---</p>
+              </div>
+            </div>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6'>
+              <div className='my-4 flex size-fit justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+                <LineComponent />
+              </div>
+              <div className='my-4 flex size-fit justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+                <AreaChartComponent />
+              </div>
+            </div>
+            <div className='m-4 flex size-fit flex-col justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+              <div>
+                <h1>Radar Chart for comparision</h1>
+                <p className='mt-5'>Some description about the chart</p>
+              </div>
+              <DonutComponent />
+            </div>
+
+            <div className=' grid grid-flow-row grid-cols-2 gap-2 '>
+              <div className='m-4 flex size-fit flex-col justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+                <div>
+                  <h1>Pie Chart</h1>
+                  <p className='mt-5'>Some description about the chart</p>
+                </div>
+                <PieChartComponent />
+              </div>
+              <div className='m-4 flex size-fit flex-col justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+                <div>
+                  <h1>Radar Chart for comparision</h1>
+                  <p className='mt-5'>Some description about the chart</p>
+                </div>
+                <RadarChartComponent />
+              </div>
+
+              <div className='m-4 flex size-fit flex-col justify-between rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+                <div>
+                  <h1>Radar Chart for comparision</h1>
+                  <p className='mt-5'>Some description about the chart</p>
+                </div>
+                <SkillsRadarComponent />
+              </div>
+            </div>
+            <div className='m-4 flex size-fit flex-col justify-center rounded-2xl p-4 text-slate-50 shadow-md shadow-[#6B37CA]  backdrop-blur-md'>
+              <div>Login Data Of Ram of a week</div>
+              <BubbleChartComponent />
+            </div>
+          </div>
+        ) : selected === 1 ? (
+          <div>Calendar</div>
+        ) : selected === 2 ? (
+          <div>Profile</div>
+        ) : selected === 3 ? (
+          <div>Settings</div>
+        ) : selected === 4 ? (
+          <div>Chart</div>
+        ) : (
+          <div>OK</div>
+        )}
+      </div>
+    </DndContext>
   )
 }
 
