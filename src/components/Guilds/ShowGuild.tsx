979cb29f683f2343a369a172d77bc0619483f0f0
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-03 15:11:03 +0545

    added hover effect for the images in guilds page

--- src/components/Guilds/ShowGuild.tsx
+++ src/components/Guilds/ShowGuild.tsx
@@ -15,17 +15,17 @@ export default function ShowGuild({ users, filter, searchTerm }: { users: any; f
             <a
               href={`/regions/${user.name.toLowerCase().replace(' ', '-')}`}
               key={index}
-              className='relative flex h-[280px] w-[240px] flex-col items-center justify-center rounded-bl-lg rounded-tr-lg  transition duration-500 ease-out hover:scale-105'
+              className='relative flex h-[280px] w-[240px] flex-col items-center justify-center rounded-bl-lg rounded-tr-lg bg-purple-950/20 transition duration-500 ease-out'
             >
               <Image
-                className='absolute top-0 w-[240px] rounded-tr-md'
+                className='absolute top-0 w-[240px] rounded-tr-md transition duration-500 ease-out hover:-translate-y-3 hover:scale-110'
                 src={user.avatarimg}
                 alt=''
                 width={240}
                 height={280}
                 style={{
                   objectFit: 'cover',
-                  filter: `drop-shadow(-4px -2px 3px rgba(${
+                  filter: `drop-shadow(-1px -2px 4px rgba(${
                     user.guild === 'PADMA'
                       ? '255, 0, 0, 0.5'
                       : user.guild === 'VAJRA'
@@ -37,29 +37,44 @@ export default function ShowGuild({ users, filter, searchTerm }: { users: any; f
                             : '255, 255, 255, 0.5'
                   }))`,
                 }}
+                //On hover remove the drop shadow
+                onMouseOver={(e) => {
+                  e.currentTarget.style.filter = 'none'
+                }}
+                onMouseOut={(e) => {
+                  e.currentTarget.style.filter = `drop-shadow(-1px -2px 4px rgba(${
+                    user.guild === 'PADMA'
+                      ? '255, 0, 0, 0.5'
+                      : user.guild === 'VAJRA'
+                        ? '0, 0, 255, 0.5'
+                        : user.guild === 'RATNA'
+                          ? '255, 255, 0, 0.5'
+                          : user.guild === 'KARMA'
+                            ? '0, 255, 0, 0.5'
+                            : '255, 255, 255, 0.5'
+                  }))`
+                }}
               />
-              {/* <span className='absolute right-2 top-2'>
-                {user.guild === 'PADMA' ? (
-                  <div className='size-5 rounded-full bg-red-500'></div>
-                ) : user.guild === 'VAJRA' ? (
-                  ''
-                ) : user.guild === 'RATNA' ? (
-                  <div className='size-5 rounded-full bg-yellow-500'></div>
-                ) : user.guild === 'KARMA' ? (
-                  ''
-                ) : user.guild === 'BUDDHA' ? (
-                  ''
-                ) : (
-                  ''
-                )}
-              </span> */}
-              <span className='absolute top-0 flex size-full items-center justify-center opacity-0 transition duration-500 ease-out hover:bg-black/80 hover:opacity-100'>
+              {/* <span className='absolute top-0 flex size-full items-center justify-center opacity-0 transition duration-500 ease-out hover:bg-black/80 hover:opacity-100'>
                 <p>{user.description}</p>
-              </span>
-              <span
-                className={`absolute bottom-0 flex w-full flex-col items-center rounded-bl-md bg-purple-950 px-3 py-2 ${user.guild === 'PADMA' ? '' : user.guild === 'VAJRA' ? ' text-blue-300 shadow-blue-700 [text-shadow:1px_1px_2px_var(--tw-shadow-color)]' : user.guild === 'RATNA' ? 'text-yellow-300' : user.guild === 'KARMA' ? 'text-green-300' : user.guild === 'BUDDHA' ? 'text-gray-300' : 'text-white'}`}
-              >
-                <h1 className='font-bold transition duration-300 ease-in-out '>{user.name}</h1>
+              </span> */}
+              <span className={`absolute bottom-0 flex w-full items-center rounded-bl-md bg-purple-950 px-3 py-2`}>
+                <h1 className='flex w-full items-center justify-between gap-x-4 font-bold transition duration-300 ease-in-out'>
+                  {user.name.toUpperCase()}
+                  {user.continent.toUpperCase() === 'EAST-ASIA' ? (
+                    <p className='size-2 rounded-full bg-green-500'></p>
+                  ) : user.guild === 'VAJRA' ? (
+                    ''
+                  ) : user.guild === 'RATNA' ? (
+                    <p className='size-2 rounded-full bg-yellow-500'></p>
+                  ) : user.guild === 'KARMA' ? (
+                    ''
+                  ) : user.guild === 'BUDDHA' ? (
+                    ''
+                  ) : (
+                    ''
+                  )}
+                </h1>
               </span>
             </a>
           ))}
