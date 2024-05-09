e538c2c46dc5b2ff60a544a7f7ff9416dd97000c
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-09 16:29:59 +0545

    added one more view in regions page

--- src/components/Regions/ShowRegion2.tsx
+++ src/components/Regions/ShowRegion2.tsx
@@ -4,10 +4,14 @@ export default function ShowRegion2({ filter, searchTerm }: { filter: string; se
   return (
     <>
       <div className='relative flex flex-col'>
-        <Image src='/svgs/world.svg' width={3000} height={1000} alt='world map' />
-        <div className='flex h-48 items-center justify-center bg-black/50 lg:absolute lg:-bottom-10 lg:right-20 lg:w-[90%]'>
-          a
+        {/* <Image src='/svgs/world.svg' width={3000} height={500} alt='world map' /> */}
+        <div className='flex h-[500px] w-full justify-end pr-5'>
+          <Image src='/svgs/na.svg' width={500} height={500} alt='world map' />
+          <div className='mx-10 flex h-full w-[40%] items-center justify-center rounded-md bg-black/50'>a</div>
         </div>
+        {/* <div className='flex h-48 items-center justify-center bg-black/50 lg:absolute lg:-bottom-10 lg:right-20 lg:w-[90%]'>
+          a
+        </div> */}
       </div>
     </>
   )
