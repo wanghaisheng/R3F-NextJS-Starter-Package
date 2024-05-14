048789f442ae9f890b127f57041d43eda4891dee
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-05-14 13:00:36 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -58,28 +58,28 @@ const nextConfig = {
           minimizer.options.terserOptions.module = true
         }
       })
-      config.plugins.push(
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: path.join(__dirname, 'node_modules/cesium/Build/Cesium/Workers'),
-              to: '../public/Cesium/Workers',
-            },
-            {
-              from: path.join(__dirname, 'node_modules/cesium/Build/Cesium/ThirdParty'),
-              to: '../public/Cesium/ThirdParty',
-            },
-            {
-              from: path.join(__dirname, 'node_modules/cesium/Build/Cesium/Assets'),
-              to: '../public/Cesium/Assets',
-            },
-            {
-              from: path.join(__dirname, 'node_modules/cesium/Build/Cesium/Widgets'),
-              to: '../public/Cesium/Widgets',
-            },
-          ],
-        }),
-      )
+      // config.plugins.push(
+      //   new CopyWebpackPlugin({
+      //     patterns: [
+      //       {
+      //         from: path.join(__dirname, 'node_modules/cesium/Build/Cesium/Workers'),
+      //         to: '../public/Cesium/Workers',
+      //       },
+      //       {
+      //         from: path.join(__dirname, 'node_modules/cesium/Build/Cesium/ThirdParty'),
+      //         to: '../public/Cesium/ThirdParty',
+      //       },
+      //       {
+      //         from: path.join(__dirname, 'node_modules/cesium/Build/Cesium/Assets'),
+      //         to: '../public/Cesium/Assets',
+      //       },
+      //       {
+      //         from: path.join(__dirname, 'node_modules/cesium/Build/Cesium/Widgets'),
+      //         to: '../public/Cesium/Widgets',
+      //       },
+      //     ],
+      //   }),
+      // )
       config.plugins.push(
         new webpack.DefinePlugin({
           CESIUM_BASE_URL: JSON.stringify('/Cesium'),
