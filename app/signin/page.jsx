999b0760e2f8ea355b937ecf186789489c47815b
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-21 14:33:30 +0545

    improvised the signin and signup page

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,6 +1,7 @@
 'use client'
-import { LogosGoogleIcon } from '@/logo/LogosGoogleIcon'
-import { LogosApple } from '@/logo/LogosApple'
+
+import { FcGoogle } from 'react-icons/fc'
+import { FaApple } from 'react-icons/fa'
 import { LogosFacebook } from '@/logo/LogosFacebook'
 import { CardBody, CardContainer, CardItem } from '@/components/card/card'
 import Image from 'next/image'
@@ -20,8 +21,34 @@ const SignIn = () => {
   const [password, setPassword] = useState('')
   const { updateUser } = useUser()
 
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
+
+  // const validateEmail = (email) => {
+  //   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  //   return re.test(String(email).toLowerCase())
+  // }
+
   const handleSubmit = async (event) => {
     event.preventDefault()
+    let valid = true
+
+    // if (!validateEmail(email)) {
+    //   setEmailError('Invalid email address.')
+    //   valid = false
+    // } else {
+    //   setEmailError('')
+    // }
+
+    if (password.length < 3) {
+      setPasswordError('Password must be at least 3 characters long.')
+      valid = false
+    } else {
+      setPasswordError('')
+    }
+
+    if (!valid) return
+
     const submit = {
       email,
       password,
@@ -56,7 +83,7 @@ const SignIn = () => {
           transition={{ duration: 0.5, delay: 0.3 }}
           className='hidden items-center justify-center pl-10 sm:flex'
         >
-          <CardContainer className='px-10 py-0 hover:shadow-3xl dark:border-none dark:hover:border-none dark:hover:shadow-3xl'>
+          <CardContainer className='px-10 py-0  dark:border-none dark:hover:border-none'>
             <CardBody className='group/card relative size-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 sm:w-[30rem] dark:border-white/[0.2] dark:bg-black dark:hover:shadow-3xl dark:hover:shadow-emerald-500/[0.1]'>
               <div className='flex'>
                 <CardItem className='mt-4 w-full'>
@@ -93,58 +120,65 @@ const SignIn = () => {
               <h2 className='p-2 text-center text-xl text-purple-400'>SIGN IN</h2>
             </div>
             <form action='#' className='flex flex-col items-center justify-center gap-2 p-3'>
-              <label htmlFor='' className='text-xl font-semibold '>
+              <label htmlFor='email' className='text-xl font-semibold '>
                 Email
               </label>
-              <div className='input-group m-2 flex rounded-md '>
-                <div className='input-icon text-black '>
+              <div className={`input-group m-2 flex rounded-md border-2  ${emailError ? ' border-red-500' : ''}`}>
+                <div className='input-icon text-black'>
                   <UserLogoIcon />
                 </div>
                 <input
                   type='email'
+                  id='email'
                   name='email'
-                  className='rounded-md  p-2 text-black'
+                  className={`rounded-md p-2 text-black`}
                   value={email}
                   placeholder='Email'
-                  onChange={({ target }) => setEmail(target?.value)}
+                  onChange={({ target }) => setEmail(target.value)}
                 />
               </div>
 
-              <label htmlFor='' className='text-xl font-semibold'>
+              <label htmlFor='password' className='text-xl font-semibold'>
                 Password
               </label>
-              <div className='input-group m-2 flex rounded-md'>
+              <div className={`input-group m-2 flex rounded-md border-2 ${passwordError ? ' border-red-500' : ''}`}>
                 <div className='input-icon text-black'>
                   <PasswordLogoIcon />
                 </div>
                 <input
                   type='password'
+                  id='password'
                   name='password'
                   className='rounded-md p-2 text-black'
                   placeholder='Password'
                   value={password}
-                  onChange={({ target }) => setPassword(target?.value)}
+                  onChange={({ target }) => setPassword(target.value)}
                 />
               </div>
+
               <div className=''>
                 <p className='flex justify-between text-sm text-blue-500'>
-                  <a href='' className='text-start' style={{ marginRight: '2.75rem' }}>
+                  <a
+                    href=''
+                    className='text-start transition-colors hover:text-blue-700'
+                    style={{ marginRight: '2.75rem' }}
+                  >
                     Mobile Sign In
                   </a>
-                  <a href='' className='text-end'>
+                  <a href='' className='text-end transition-colors hover:text-blue-700'>
                     Forgot Password?
                   </a>
                 </p>
               </div>
 
-              <div className='signup-btn flex w-full items-center justify-center p-5'>
+              <div className='flex w-full items-center justify-center p-5'>
                 <motion.button
                   whileHover={{ scale: 1.05 }}
                   whileTap={{ scale: 0.95 }}
                   onClick={handleSubmit}
-                  className='w-full rounded-2xl bg-purple-200 p-2 px-4 text-black'
+                  className='w-full rounded-lg bg-purple-200 p-2 px-4 text-black'
                 >
-                  SignIn
+                  Sign In
                 </motion.button>
               </div>
             </form>
@@ -156,19 +190,19 @@ const SignIn = () => {
             </div>
             <div className='flex justify-center gap-16 p-5'>
               <a href='/api/auth/signin'>
-                <LogosGoogleIcon className='logos text-2xl' />
+                <FcGoogle className='text-3xl transition-transform hover:scale-125' />
               </a>
               <a href=''>
-                <LogosApple className='logos text-2xl' />
+                <FaApple className='text-3xl transition-transform hover:scale-125' />
               </a>
               <a href=''>
-                <LogosFacebook className='logos text-2xl' />
+                <LogosFacebook className='text-3xl transition-transform hover:scale-125' />
               </a>
             </div>
             <div className='m-5 flex items-center justify-center '>
               <p className=' text-sm'>
                 Not a Genius User yet?
-                <a href='/signup' className='ml-1 text-blue-500'>
+                <a href='/signup' className='ml-1 text-blue-500 transition-colors hover:text-blue-700'>
                   Sign Up Now
                 </a>
               </p>
