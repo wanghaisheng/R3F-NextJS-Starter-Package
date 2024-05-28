14fd7eb58e9778172ee69f4faa05d395941a2be4
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-28 16:52:36 +0545

    added filler in regions page

--- app/avatars/page.jsx
+++ app/avatars/page.jsx
@@ -1,7 +1,14 @@
 export default function Avatars() {
   return (
-    <div className='flex items-center justify-center'>
-      <h1 className='text-5xl text-white'> Comming Soon!!!</h1>
+    <div className='relative h-[80vh] flex-1 items-center justify-center'>
+      <div className='absolute top-10 z-50 flex w-full justify-center'>
+        <p className='animate-pulse rounded-lg p-2 font-semibold  text-purple-200 shadow shadow-violet-400'>
+          BETA TESTING
+        </p>
+      </div>
+      <div className='flex size-full animate-pulse items-center justify-center'>
+        <h1 className='text-3xl font-bold text-white'> COMMING SOON!!!</h1>
+      </div>
     </div>
   )
 }
