2a65d51111d482b8501d6ae25f5942ddeddba03a
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-06-11 14:50:12 +0545

    added a sos button in emergency tab

--- src/components/RightSidebarComponent/SubComponents/EmergencyComponent.tsx
+++ src/components/RightSidebarComponent/SubComponents/EmergencyComponent.tsx
@@ -26,22 +26,37 @@ export default function EmergencyComponent({ setActiveTab, setShowSignUp }) {
       {user ? (
         <>
           {user.cards[0] ? (
-            <div className='group h-[239px] [prespective:1000px]'>
-              <EmergencyCard
-                type='emergency'
-                isFlipped={isFlipped}
-                handleFlip={handleFlip}
-                user={user}
-                name={user.first_name + ' ' + user.last_name}
-                blood_group={user.cards[0].blood_group}
-                emergency_contact={user.cards[0].emergency_contact}
-                emergency_details={user.cards[0].emergency_details}
-                emergency_address={user.cards[0].emergency_address}
-                imgSrc={imgSrc}
-              />
-            </div>
+            <>
+              <div className='group h-[239px] [prespective:1000px]'>
+                <EmergencyCard
+                  type='emergency'
+                  isFlipped={isFlipped}
+                  handleFlip={handleFlip}
+                  user={user}
+                  name={user.first_name + ' ' + user.last_name}
+                  blood_group={user.cards[0].blood_group}
+                  emergency_contact={user.cards[0].emergency_contact}
+                  emergency_details={user.cards[0].emergency_details}
+                  emergency_address={user.cards[0].emergency_address}
+                  imgSrc={imgSrc}
+                />
+              </div>
+              <div className='mt-4 flex items-center justify-center'>
+                <button
+                  className='w-full rounded bg-red-700 p-2 text-xl font-bold transition-all duration-300 hover:bg-red-700/40'
+                  onClick={() =>
+                    alert('This will share the current location of the user and call the emergency contact')
+                  }
+                >
+                  SOS
+                </button>
+              </div>
+            </>
           ) : (
-            <div>Okay</div>
+            <>
+              <h2>You have not filled Emergency details yet.</h2>
+              <p>Add emergency details</p>
+            </>
           )}
         </>
       ) : (
