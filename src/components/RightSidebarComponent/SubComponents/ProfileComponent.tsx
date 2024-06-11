ad68285e553f7e96133ee0dbe11371de9f080cba
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-06-11 14:59:40 +0545

    added avatar component in the sidebar profile tab

--- src/components/RightSidebarComponent/SubComponents/ProfileComponent.tsx
+++ src/components/RightSidebarComponent/SubComponents/ProfileComponent.tsx
@@ -1,6 +1,6 @@
 'use client'
-
-import { useState } from 'react'
+import dynamic from 'next/dynamic'
+import { useState, useEffect } from 'react'
 import SignUpComponent from '@/components/SignUp/SignUpComponent'
 import { FcGoogle } from 'react-icons/fc'
 import { FaApple } from 'react-icons/fa'
@@ -8,14 +8,31 @@ import SignInComponent from '@/components/SignUp/SignInComponent'
 import { useUser } from '@/context/UserContext/UserContext'
 import { LogosFacebook } from '@/logo/LogosFacebook'
 import { IoIosArrowBack } from 'react-icons/io'
+const Avatar = dynamic(() => import('@/components/Avatar').then((mod) => mod.Avatar), { ssr: false })
 
 export default function ProfileComponent({ setShowSignUp, setActiveTab }) {
   const { user } = useUser()
+  const [avatarsData, setAvatarsData] = useState([])
 
   const handleSignUpClick = () => {
     setActiveTab('search')
     setShowSignUp(true)
   }
+
+  useEffect(() => {
+    const fetchAvatarsData = async () => {
+      try {
+        setAvatarsData(user.avatar)
+      } catch (error) {
+        console.log('Error fetching avatars data:', error)
+      }
+    }
+    if (user) {
+      fetchAvatarsData() // Fetch data only if user is available
+    }
+  }, [user])
+
+  console.log(user)
 
   return (
     <div className='mb-32 flex h-full flex-col'>
@@ -26,6 +43,35 @@ export default function ProfileComponent({ setShowSignUp, setActiveTab }) {
             Welcome, {user.first_name} {user.last_name}!
           </p>
           <div>Avatar</div>
+          <div className=' h-[360px] w-full '>
+            {avatarsData && avatarsData.length !== 0 ? (
+              <Avatar
+                modelSrc={`${avatarsData.slice(-1)[0].avatar_url}`}
+                shadows
+                animationSrc='/male-spawn-animation.fbx'
+                style={{ background: 'rgb(9,20,26)', pointerEvents: 'none' }}
+                fov={40}
+                cameraTarget={1.5}
+                cameraInitialDistance={30}
+                effects={{
+                  ambientOcclusion: true,
+                }}
+              />
+            ) : (
+              <Avatar
+                modelSrc='https://models.readyplayer.me/658be9e8fc8bec93d06806f3.glb?morphTargets=ARKit,Eyes Extra&textureAtlas=1024&pose=A&useHands=true'
+                shadows
+                animationSrc='/male-idle-3.fbx'
+                style={{ background: 'rgb(9,20,26)', pointerEvents: 'none' }}
+                fov={40}
+                cameraTarget={1.5}
+                cameraInitialDistance={30}
+                effects={{
+                  ambientOcclusion: true,
+                }}
+              />
+            )}
+          </div>
           <p>Other details</p>
           <p>Input to add bio</p>
           <p>Input to take profile picture</p>
