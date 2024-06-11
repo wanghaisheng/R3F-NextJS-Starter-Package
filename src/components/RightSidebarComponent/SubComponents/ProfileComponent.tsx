14b3581474c5f48ac21d288c0fb62c5cf4368c35
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-06-11 16:15:58 +0545

    minor updates

--- src/components/RightSidebarComponent/SubComponents/ProfileComponent.tsx
+++ src/components/RightSidebarComponent/SubComponents/ProfileComponent.tsx
@@ -1,18 +1,14 @@
 'use client'
 import dynamic from 'next/dynamic'
-import { useState, useEffect } from 'react'
-import SignUpComponent from '@/components/SignUp/SignUpComponent'
-import { FcGoogle } from 'react-icons/fc'
-import { FaApple } from 'react-icons/fa'
-import SignInComponent from '@/components/SignUp/SignInComponent'
+import { useState, useEffect, useRef } from 'react'
 import { useUser } from '@/context/UserContext/UserContext'
-import { LogosFacebook } from '@/logo/LogosFacebook'
-import { IoIosArrowBack } from 'react-icons/io'
 const Avatar = dynamic(() => import('@/components/Avatar').then((mod) => mod.Avatar), { ssr: false })
 
 export default function ProfileComponent({ setShowSignUp, setActiveTab }) {
   const { user } = useUser()
   const [avatarsData, setAvatarsData] = useState([])
+
+  console.log('user:', user)
 
   const handleSignUpClick = () => {
     setActiveTab('search')
@@ -32,13 +28,10 @@ export default function ProfileComponent({ setShowSignUp, setActiveTab }) {
     }
   }, [user])
 
-  console.log(user)
-
   return (
     <div className='mb-32 flex h-full flex-col'>
       {user ? (
         <div className='flex-1 items-center justify-center rounded-lg bg-black text-white'>
-          {/* User is signed in, show a message or other content */}
           <p>
             Welcome, {user.first_name} {user.last_name}!
           </p>
@@ -73,12 +66,11 @@ export default function ProfileComponent({ setShowSignUp, setActiveTab }) {
             )}
           </div>
           <p>Other details</p>
-          <p>Input to add bio</p>
-          <p>Input to take profile picture</p>
+          <p>Form to add profile pic and bio</p>
         </div>
       ) : (
         <>
-          <div>You must signin to view this tab</div>
+          <div>You must sign in to view this tab</div>
           <div
             onClick={handleSignUpClick}
             className='mt-2 flex cursor-pointer justify-center rounded border border-purple-700 bg-purple-800/30 p-2 transition-colors hover:bg-purple-800/40 hover:text-purple-200'
