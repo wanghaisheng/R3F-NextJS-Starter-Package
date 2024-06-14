20731f48c6d1e21b0b35153b2324b3793e045b82
SatkarNiraula <satkarniraula69@gmail.com>
2024-06-14 14:41:11 +0545

    user age in response and user update bug fix

--- app/api/public/users/[username]/route.js
+++ app/api/public/users/[username]/route.js
@@ -8,6 +8,24 @@ export async function GET(request, { params }) {
   try {
     const username = params.username
 
+    function calculateAge(dob) {
+      // Convert the date of birth from string to Date object
+      const birthDate = new Date(dob)
+      // Get the current date
+      const currentDate = new Date()
+
+      // Calculate the age
+      let age = currentDate.getFullYear() - birthDate.getFullYear()
+      const monthDiff = currentDate.getMonth() - birthDate.getMonth()
+
+      // If the birth month hasn't occurred yet this year or it's the birth month but the day hasn't occurred yet, subtract 1 from age
+      if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())) {
+        age--
+      }
+
+      return age
+    }
+
     // Fetch the user by ID
     const user = await prisma.users.findUnique({
       where: { username: username },
@@ -22,6 +40,7 @@ export async function GET(request, { params }) {
         guild_id: true,
         address: true,
         region: true,
+        dob: true,
         cards: true,
         experience: true,
         avatar: true,
@@ -29,11 +48,28 @@ export async function GET(request, { params }) {
       },
     })
 
+    const userResponse = {
+      first_name: user.first_name,
+      last_name: user.last_name,
+      username: user.username,
+      email: user.email,
+      image_urls: user.image_urls,
+      description: user.description,
+      guild_id: user.guild_id,
+      address: user.address,
+      region: user.region,
+      age: calculateAge(user.dob),
+      cards: user.cards,
+      experience: user.experience,
+      avatar: user.avatar,
+      skills: user.skills,
+    }
+
     if (!user) {
       return NextResponse.error('User not found', 404)
     }
 
-    return NextResponse.json(user)
+    return NextResponse.json(userResponse)
   } catch (error) {
     console.error('Error fetching user', error)
     return NextResponse.error('Internal Server Error', 500)
