d1c4397de72e01789ac90d44493968c9ef4242b8
SatkarNiraula <satkarniraula69@gmail.com>
2024-06-17 14:08:12 +0545

    sign in api bug fix

--- app/api/internal/signin/route.js
+++ app/api/internal/signin/route.js
@@ -8,19 +8,30 @@ const JWT_SECRET = process.env.JWT_SECRET
 
 export async function POST(request) {
   try {
-    const { username, phone_number, email, password } = await request.json()
+    const { username, email, password } = await request.json()
 
-    // Find the user by email
-    const user = await prisma.users.findUnique({
+    // Build an array of conditions, including only those that are defined
+    const conditions = [
+      username && { username },
+      email && { email },
+      // phone_number && { phone_number } // Include phone_number if needed
+    ].filter(Boolean) // Filter out any undefined or false values
+
+    // If no valid conditions are provided, throw an error
+    if (conditions.length === 0) {
+      throw new Error('At least one of username or email must be provided')
+    }
+
+    // Query the database with the valid conditions using findFirst
+    const user = await prisma.users.findFirst({
       where: {
-        OR: [{ username: username }, { email: email }, { phone_number: phone_number }],
+        OR: conditions,
       },
     })
 
     // Check if user exists
     if (!user) {
       // If user doesn't exist, return error indicating incorrect email
-
       return NextResponse.json({ error: 'User does not exist' }, { status: 404 })
     }
 
@@ -28,7 +39,6 @@ export async function POST(request) {
     const passwordMatch = await bcrypt.compare(password, user.password)
     if (!passwordMatch) {
       // If password doesn't match, return error indicating incorrect password
-
       return NextResponse.json({ error: 'Incorrect password' }, { status: 401 })
     }
 
@@ -39,7 +49,6 @@ export async function POST(request) {
     return NextResponse.json({ token })
   } catch (error) {
     console.error('Error signing in:', error)
-
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
   }
 }
