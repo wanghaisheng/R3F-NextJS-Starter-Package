5700a0a855a2106c223f517e2fe7030a5e2a9366
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-06-19 16:15:50 +0545

    updated icons in regions header

--- src/components/Regions/RegionHeader.tsx
+++ src/components/Regions/RegionHeader.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import Image from 'next/image'
+import { FaGlobeAfrica, FaGlobeEurope, FaGlobeAmericas, FaGlobeAsia } from 'react-icons/fa'
+import { HiGlobeAsiaAustralia } from 'react-icons/hi2'
+import { BsGlobeCentralSouthAsia } from 'react-icons/bs'
 
 export default function RegionHeader({ onFilterChange }: { onFilterChange: (filter: string) => void }) {
   const [isSmallScreen, setIsSmallScreen] = useState(false)
@@ -49,7 +51,7 @@ export default function RegionHeader({ onFilterChange }: { onFilterChange: (filt
               className={`cursor-pointer text-green-300 transition duration-300 ease-out hover:scale-105 hover:text-purple-300 ${activeFilter === 'AFRICA' && 'text-green-700'}`}
               onClick={() => handleFilterClick('AFRICA')}
             >
-              AF
+              <FaGlobeAfrica />
             </a>
             <a
               className={`cursor-pointer text-red-300 transition duration-300 ease-out hover:scale-105 hover:text-purple-300 ${activeFilter === 'EUROPE' && 'text-red-500'}`}
@@ -73,69 +75,83 @@ export default function RegionHeader({ onFilterChange }: { onFilterChange: (filt
         )}
       </div>
       {/* Sidebar */}
-      <div className='fixed top-0 hidden items-center justify-start  font-semibold lg:flex'>
+      <div className='fixed top-0 hidden items-center justify-start font-semibold lg:flex'>
         <ul className='flex h-screen w-[330px] flex-col gap-y-2 overflow-hidden bg-gradient-to-r from-black/80 text-white'>
           <li className='mt-[40%]'>
             <a
-              className={`flex cursor-pointer items-center gap-x-3 pl-5 transition duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'ASIA' && 'bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
+              className={`flex cursor-pointer items-center gap-x-7 py-3 pl-10 transition-all duration-300 ease-out hover:scale-105 hover:text-purple-300 ${activeFilter === 'ASIA' && 'gap-x-9 bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
               onClick={() => handleFilterClick('ASIA')}
             >
-              <Image src='/continents/eu.png' height={20} width={20} alt='as' className='m-5' />
+              <FaGlobeAsia
+                className={`size-7 rounded-full ${activeFilter === 'ASIA' && 'scale-110 bg-gradient-to-r from-[#FFD700] to-[#544a7d] text-[#000046]'} transition-all duration-300`}
+              />
               ASIA
             </a>
           </li>
 
           <li>
             <a
-              className={`flex cursor-pointer items-center gap-x-3 pl-5 transition duration-300 ease-out hover:scale-105 hover:text-purple-300 ${activeFilter === 'NORTH AMERICA' && 'bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
+              className={`flex cursor-pointer items-center gap-x-7 py-3 pl-10 transition-all duration-300 ease-out hover:scale-105 hover:text-purple-300 ${activeFilter === 'NORTH AMERICA' && 'gap-x-9 bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
               onClick={() => handleFilterClick('NORTH AMERICA')}
             >
-              <Image src='/continents/na.png' height={20} width={20} alt='na' className='m-5' />
+              <FaGlobeAmericas
+                className={`size-7 rounded-full ${activeFilter === 'NORTH AMERICA' && 'scale-110 bg-gradient-to-r from-[#52c234] to-[#061700] text-[#000046]'} transition-all duration-300`}
+              />
               NORTH AMERICA
             </a>
           </li>
           <li>
             <a
-              className={`flex cursor-pointer items-center gap-x-3 pl-5 transition duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'SOUTH AMERICA' && 'bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
+              className={`flex cursor-pointer items-center gap-x-7 py-3 pl-10 transition-all duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'SOUTH AMERICA' && 'gap-x-9 bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
               onClick={() => handleFilterClick('SOUTH AMERICA')}
             >
-              <Image src='/continents/sa.png' height={20} width={20} alt='sa' className='m-5' />
+              <FaGlobeAmericas
+                className={`size-7 rounded-full ${activeFilter === 'SOUTH AMERICA' && 'scale-110 bg-gradient-to-r from-[#FF0099] to-[#493240] text-[#000046]'} transition-all duration-300`}
+              />
               SOUTH AMERICA
             </a>
           </li>
           <li>
             <a
-              className={`flex cursor-pointer items-center gap-x-3 pl-5 transition duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'AFRICA' && 'bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
+              className={`flex cursor-pointer items-center gap-x-7 py-3 pl-10 transition-all duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'AFRICA' && 'gap-x-9 bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
               onClick={() => handleFilterClick('AFRICA')}
             >
-              <Image src='/continents/af.png' height={20} width={20} alt='af' className='m-5' />
+              <FaGlobeAfrica
+                className={`size-7 rounded-full ${activeFilter === 'AFRICA' && 'scale-110 bg-gradient-to-r from-[#f12711] to-[#f5af19] text-[#000046]'} transition-all duration-300`}
+              />
               AFRICA
             </a>
           </li>
           <li>
             <a
-              className={`flex cursor-pointer items-center gap-x-3 pl-5 transition duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'EUROPE' && 'bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
+              className={`flex cursor-pointer items-center gap-x-7 py-3 pl-10 transition-all duration-300 ease-in-out hover:scale-105 hover:text-purple-300 ${activeFilter === 'EUROPE' && 'gap-x-9  bg-gradient-to-r from-purple-400/50 text-purple-400'}`}
               onClick={() => handleFilterClick('EUROPE')}
             >
-              <Image src='/continents/eu.png' height={20} width={20} alt='eu' className='m-5' />
+              <FaGlobeEurope
+                className={`size-7 rounded-full ${activeFilter === 'EUROPE' && 'scale-110  bg-gradient-to-r from-[#1488CC] to-[#2B32B2] text-[#000046]'} transition-all duration-300`}
+              />
               EUROPE
             </a>
           </li>
           <li>
             <a
-              className={`flex cursor-pointer items-center gap-x-3 pl-5 transition duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'AUSTRALIA & OCEANIA' && 'bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
+              className={`flex cursor-pointer items-center gap-x-7 py-3 pl-10 transition-all duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'AUSTRALIA & OCEANIA' && 'gap-x-9 bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
               onClick={() => handleFilterClick('AUSTRALIA & OCEANIA')}
             >
-              <Image src='/continents/oc.png' height={20} width={20} alt='oc' className='m-5' />
+              <HiGlobeAsiaAustralia
+                className={`size-7 rounded-full ${activeFilter === 'AUSTRALIA & OCEANIA' && 'scale-110 bg-gradient-to-r from-[#c31432] to-[#240b36] text-[#000046]'} transition-all duration-300`}
+              />
               AUSTRALIA & OCEANIA
             </a>
           </li>
           <li>
             <a
-              className={`flex cursor-pointer items-center gap-x-3 pl-5 transition duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'ANTARCTICA' && 'bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
+              className={`flex cursor-pointer items-center gap-x-7 py-3 pl-10 transition-all duration-300 ease-out hover:scale-105 hover:text-purple-300  ${activeFilter === 'ANTARCTICA' && 'gap-x-9 bg-gradient-to-r from-purple-700/30 text-purple-200'}`}
               onClick={() => handleFilterClick('ANTARCTICA')}
             >
-              <Image src='/continents/na.png' height={20} width={20} alt='an' className='m-5' />
+              <BsGlobeCentralSouthAsia
+                className={`size-7 rounded-full ${activeFilter === 'ANTARCTICA' && 'scale-110  bg-gradient-to-r from-[#6DD5FA] to-[#FFFFFF] text-[#000046]'} transition-all duration-300`}
+              />
               ANTARCTICA
             </a>
           </li>
