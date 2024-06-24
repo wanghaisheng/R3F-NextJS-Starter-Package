3e2c903486676417b22a72b4f80e043be787ca54
SatkarNiraula <satkarniraula69@gmail.com>
2024-06-24 18:29:51 +0545

    minor bug fix

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,11 +1,11 @@
 'use server'
 import { revalidateTag } from 'next/cache'
-import { redirect } from 'next/navigation'
+
 export async function revalidateUser() {
   console.log('Revalidating user cache')
   try {
     revalidateTag('user')
-    redirect('/')
+    console.log('Revalidated successfully')
   } catch (error) {
     console.error('Revalidation error:', error)
   }
