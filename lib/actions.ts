81c4d857ba9f1f317a936691a0ec0e91585d6306
SatkarNiraula <satkarniraula69@gmail.com>
2024-06-24 17:43:27 +0545

    test

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,7 +1,12 @@
 'use server'
 import { revalidateTag } from 'next/cache'
-
+import { redirect } from 'next/navigation'
 export async function revalidateUser() {
-console.log('Revalidating user cache')
-  return revalidateTag('user')
+  console.log('Revalidating user cache')
+  try {
+    revalidateTag('user')
+    redirect('/')
+  } catch (error) {
+    console.error('Revalidation error:', error)
+  }
 }
