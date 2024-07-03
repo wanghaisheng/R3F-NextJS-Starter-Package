0b86dd480f8b2eaf1b47722223fc85162e777d88
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-07-03 14:32:17 +0545

    fix:: fixed the colors and layout shit of homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,14 +3,13 @@ import '@/global.css'
 // import { UserProvider } from '@/context/UserContext/UserContext'
 import UserProvider from './UserProvider'
 import StarsCanvas from '@/components/StarsCanvas/StarBackground'
-import MainContent from '@/components/MainContent/MainContent'
 
 export const metadata = {
   title: 'Going Genius Next.js+ReactThreeFiber+Visage Starter Bundle',
   description: 'A minimal starter for Nextjs + React-three-fiber and Visage',
 }
 
-export default function RootLayout({ children, user }) {
+export default function RootLayout({ children }) {
   return (
     <html lang='en' className='antialiased'>
       {/*
@@ -22,9 +21,7 @@ export default function RootLayout({ children, user }) {
       <body>
         {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
         <UserProvider>
-          <Layout>
-            <MainContent>{children}</MainContent>
-          </Layout>
+          <Layout>{children}</Layout>
         </UserProvider>
       </body>
     </html>
