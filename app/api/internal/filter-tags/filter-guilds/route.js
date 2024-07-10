270598d3c136fd3f666de2339621860a95f6cf82
SatkarNiraula <satkarniraula69@gmail.com>
2024-07-10 17:09:10 +0545

    merge ::  merge with ro_.sth changes

--- app/api/internal/filter-tags/filter-guilds/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-export async function GET(request, { params }) {
-  try {
-  } catch (error) {}
-}
