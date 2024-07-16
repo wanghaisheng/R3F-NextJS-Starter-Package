71eebefc0063fce423d04354c7f4968e5d84b561
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-07-16 15:07:10 +0545

    fix:: fixed some minor issues

--- src/components/MyComponents/SearchComponent.tsx
+++ src/components/MyComponents/SearchComponent.tsx
@@ -30,7 +30,7 @@ export default function SearchComponent({
           type='search'
           placeholder='SEARCH'
           onChange={handleSearchChange}
-          className='w-full rounded-full bg-white/20 py-2 pl-12 pr-4 text-white shadow backdrop-blur-sm transition-all duration-300 ease-in-out placeholder:text-black hover:bg-white/30 hover:text-black focus:outline-none focus:ring-2 focus:ring-black/50'
+          className='w-full rounded-full bg-white/20 py-2 pl-12 pr-4 text-gray-700 shadow backdrop-blur-sm transition-all duration-300 ease-in-out placeholder:text-black hover:bg-white/30 hover:text-black focus:outline-none focus:ring-2 focus:ring-black/50'
         />
         <motion.button
           whileTap={{ scale: 0.95 }}
