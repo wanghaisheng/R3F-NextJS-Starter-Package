c981be3a1b9229560540c0da026248326c32b148
gg-ayush <97468901+gg-ayush@users.noreply.github.com>
2024-07-18 14:02:05 +0545

    api__url_change

--- app/discover/page.jsx
+++ app/discover/page.jsx
@@ -3,13 +3,12 @@ import { NextResponse } from 'next/server'
 
 async function getFacultyTags() {
   try {
-    const res = await fetch('http://localhost:3000/api/public/filter-tags/all-faculties')
+    const res = await fetch('http://r3-f-next-js-starter-package.vercel.app/api/public/filter-tags/all-faculties')
     if (!res.ok) {
       throw new Error('failed to fetch faculty tags')
     }
     return res.json()
   } catch (error) {
-    console.error(error)
     return NextResponse.error('internal server error', 500)
   }
 }
