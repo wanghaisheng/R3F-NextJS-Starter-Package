ed9741091225ad75951c8a3a96851b48883c3aab
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-07-18 15:29:51 +0545

    update:: updated the way of showing username and description

--- src/components/ProfileComponent/PublicProfileComponent/ProfileInfoComponents/LeftSideComponents/AboutUser.tsx
+++ src/components/ProfileComponent/PublicProfileComponent/ProfileInfoComponents/LeftSideComponents/AboutUser.tsx
@@ -4,6 +4,7 @@ import { useUser } from '@/UserClientProvider'
 import Link from 'next/link'
 import { useState } from 'react'
 import { MdOutlineInsertLink } from 'react-icons/md'
+import { HiPencil } from 'react-icons/hi'
 
 export default function AboutUser({ userData }) {
   const { user } = useUser()
@@ -17,14 +18,22 @@ export default function AboutUser({ userData }) {
   ]
 
   return (
-    <>
-      {/* USERNAME */}
-      <div className='my-2 flex w-full justify-center text-xl font-semibold'>
-        {userData.username.charAt(0).toUpperCase()}
-        {userData.username.slice(1)}
+    <div className='flex w-full gap-x-2'>
+      {/* Left */}
+      <div className='relative flex w-[30%] flex-col'>
+        <div>{userData.name}</div>
+        <div>Age: {userData.age}</div>
+        <div>
+          {userData.city}, {userData.country}
+        </div>
+        <div>Faculty: </div>
+
+        <div className='absolute right-1 top-1 rounded-lg bg-violet-300 p-1'>
+          <HiPencil />
+        </div>
       </div>
-      {/* Row 1 */}
-      <div className='w-full justify-center'>
+      {/* Right */}
+      <div className='relative w-[70%] justify-center rounded-lg bg-white/60 p-2'>
         <div className='flex w-full flex-wrap items-center justify-center gap-x-2'>
           <h1 className='cursor-pointer font-semibold'>
             10 <span className='text-sm font-normal hover:underline'>Followers</span>
@@ -33,45 +42,39 @@ export default function AboutUser({ userData }) {
           <h1 className='cursor-pointer font-semibold'>
             10 <span className='text-sm font-normal hover:underline'>Following</span>
           </h1>
-          <span className='text-2xl font-extrabold'>‧</span>
           <h1 className='cursor-pointer font-semibold'>
             0 <span className='text-sm font-normal hover:underline'>Connections</span>
           </h1>
-        </div>
-
-        {/* Row 2 */}
-        <div>
-          <p className='mt-2 text-sm font-light text-gray-200'>{userData.description}</p>
         </div>
         {/* Row 4 */}
         <div className='mt-2 flex items-start justify-center'>
           <MdOutlineInsertLink className='mr-1 mt-1 rotate-45 drop-shadow' size={17} />
           <div className='flex w-full flex-wrap gap-x-2 text-sm drop-shadow'>
             {Links.slice(0, 1).map((link, index) => (
-              <Link key={index} href={link.url} target='_blank' className='text-blue-300 hover:underline'>
+              <Link key={index} href={link.url} target='_blank' className='text-blue-600 hover:underline'>
                 {link.name}
               </Link>
             ))}
             {expanded &&
               Links.slice(1).map((link, index) => (
-                <Link key={index} href={link.url} target='_blank' className='text-blue-300 hover:underline'>
+                <Link key={index} href={link.url} target='_blank' className='text-blue-600 hover:underline'>
                   {link.name}
                 </Link>
               ))}
 
-            <button onClick={() => setExpanded(!expanded)} className='text-xs text-gray-200 hover:underline'>
+            <button onClick={() => setExpanded(!expanded)} className='text-xs text-gray-800 hover:underline'>
               {expanded ? 'Show Less' : `+ ${Links.length - 1} More`}
             </button>
           </div>
         </div>
         {/* Row 5 */}
         <div className='mb-3 mt-5'>
-          <p className='text-xs font-normal text-gray-200'>
-            Followed by <span className='font-bold text-white'>Person1</span> and{' '}
-            <span className='font-bold text-white'>Person2</span>
+          <p className='text-xs font-normal text-gray-900'>
+            Followed by <span className='font-bold text-blue-600'>Person1</span> and{' '}
+            <span className='font-bold text-blue-600'>Person2</span>
           </p>
         </div>
       </div>
-    </>
+    </div>
   )
 }
--- src/components/ProfileComponent/PublicProfileComponent/ProfileInfoComponents/LeftSideComponents/LeftSidePublicProfile.tsx
+++ src/components/ProfileComponent/PublicProfileComponent/ProfileInfoComponents/LeftSideComponents/LeftSidePublicProfile.tsx
@@ -22,12 +22,27 @@ export default function LeftSidePublicProfile({ user, guild }) {
   return (
     <>
       {user ? (
-        <div className='relative size-full rounded-xl bg-white/40 p-3 shadow-xl shadow-black/30 backdrop-blur-lg'>
+        <div className='relative size-full rounded-xl bg-white/30 p-3 shadow-xl shadow-black/30 backdrop-blur-lg'>
           {/* CoverPicture */}
           {/* <div className='flex w-full items-center justify-center'>
             <CoverPhoto coverPhotoUrl={user.username} height={178} />
           </div> */}
 
+          {/* USERNAME and description */}
+          <div className='absolute -top-10 left-0 flex w-full flex-col items-center justify-center'>
+            <div className='text-4xl font-bold uppercase text-white/40 drop-shadow'>{user.username}</div>
+            <div className='group flex w-1/2 justify-center overflow-hidden text-center'>
+              <div className='cursor-pointer text-sm font-light text-black'>
+                {user.description.length > 10 ? `${user.description.substring(0, 10)}...` : user.description}
+              </div>
+
+              <div className='invisible absolute top-[-80px] h-[100px] w-[200px] rounded-lg border bg-white/40 text-start text-sm font-semibold text-black group-hover:visible'>
+                {user.description}
+              </div>
+            </div>
+          </div>
+
+          {/* Avatar */}
           <div className='absolute -left-6 top-[-128px] h-[127px] w-[170px] overflow-hidden'>
             {avatar_url && (
               <div className='size-full'>
@@ -97,7 +112,7 @@ export default function LeftSidePublicProfile({ user, guild }) {
 
           {/* Guild */}
           <div className='flex flex-col pl-4 '>
-            <div className='group absolute left-4 top-4'>
+            <div className='group absolute left-0 top-0'>
               <Image
                 src={guild.find((guild) => guild.guild_name === user.guild)?.symbol || ''}
                 height={30}
