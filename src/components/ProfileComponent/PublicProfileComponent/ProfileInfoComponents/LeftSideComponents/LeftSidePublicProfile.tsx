ccb807b73f367d77c553fdfd1183ff3c88ded0be
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-07-22 11:21:12 +0545

    update:: added the gallery and skills on the left side

--- src/components/ProfileComponent/PublicProfileComponent/ProfileInfoComponents/LeftSideComponents/LeftSidePublicProfile.tsx
+++ src/components/ProfileComponent/PublicProfileComponent/ProfileInfoComponents/LeftSideComponents/LeftSidePublicProfile.tsx
@@ -18,12 +18,14 @@ export default function LeftSidePublicProfile({ user, guild }) {
     setSwitchProfile(!switchProfile)
   }
 
+  console.log('guild', guild)
+
   const avatar_url = user?.avatarurl
 
   return (
     <>
       {user ? (
-        <div className='relative size-full rounded-xl bg-white/30 p-3 shadow-xl shadow-black/30 backdrop-blur-lg'>
+        <div className='relative size-full rounded-xl bg-white/50 p-3 shadow-xl shadow-black/30 backdrop-blur-md'>
           {/* CoverPicture */}
           {/* <div className='flex w-full items-center justify-center'>
             <CoverPhoto coverPhotoUrl={user.username} height={178} />
@@ -33,10 +35,12 @@ export default function LeftSidePublicProfile({ user, guild }) {
           <div className='absolute -top-10 left-0 flex w-full flex-col items-center justify-center'>
             <div className='text-4xl font-bold uppercase text-white/40 drop-shadow'>{user.username}</div>
             <div className='group flex w-1/2 justify-center overflow-hidden text-center'>
+              {/* not hovered */}
               <div className='cursor-pointer text-sm font-light text-black'>
                 {user.description.length > 10 ? `${user.description.substring(0, 10)}...` : user.description}
               </div>
 
+              {/* pop up when hovered */}
               <div className='invisible absolute top-[-80px] h-[100px] w-[200px] rounded-lg border bg-white/40 text-start text-sm font-semibold text-black group-hover:visible'>
                 {user.description}
               </div>
@@ -56,19 +60,22 @@ export default function LeftSidePublicProfile({ user, guild }) {
                   effects={{
                     ambientOcclusion: true,
                   }}
+                  style={{
+                    filter: `drop-shadow(0px 0px 3px ${guild.find((guild) => guild.guild_name === user.guild)?.color || 'white'})`,
+                  }}
                 />
               </div>
             )}
           </div>
 
-          {/* Profile Picture And User Info */}
-          <div className='-mt-9 flex w-full select-none items-center justify-end'>
+          {/* Profile Picture */}
+          <div className='-mt-12 flex w-full select-none items-center justify-end'>
             <div
               className='relative overflow-hidden rounded-full border-[3px]'
               style={{
                 borderRadius: '50%',
-                height: 50,
-                width: 50,
+                height: 65,
+                width: 65,
               }}
               onClick={handleSwitchProfile}
             >
@@ -93,7 +100,6 @@ export default function LeftSidePublicProfile({ user, guild }) {
                   />
                 </>
               )}
-              <div className='z-20 size-full bg-white/40'></div>
             </div>
           </div>
 
@@ -108,17 +114,48 @@ export default function LeftSidePublicProfile({ user, guild }) {
           </div>
 
           {/* Pictures */}
-          <div className='flex w-full flex-col gap-y-4 p-2'>
-            {/* gallery */}
-            <div className='w-full rounded-lg bg-white/60'>
-              <div className='pl-2 text-start text-lg font-semibold text-black'>Gallery</div>
-              <Pictures user={user} user_images={user.overall_user_image} />
-            </div>
-            {/* skills */}
-            <div className='w-full rounded-lg bg-white/60'>Skills</div>
-          </div>
+          <div className='h-[300px] w-full p-2'>
+            <div className='flex size-full flex-col overflow-auto rounded-lg bg-white/60'>
+              {/* Gallery */}
+              <section>
+                <h2
+                  className='sticky top-0 z-10 pl-2 text-lg font-semibold text-white'
+                  style={{ filter: 'drop-shadow(1px 1px 1px black' }}
+                >
+                  Gallery
+                </h2>
+                <div className='w-full rounded-lg'>
+                  <Pictures user={user} user_images={user.overall_user_image} />
+                </div>
+              </section>
 
-          <div className='absolute right-6 top-36'>...</div>
+              {/* Skills */}
+              <section>
+                <h2
+                  className='sticky top-0 z-10 pl-2 text-lg font-semibold text-white'
+                  style={{ filter: 'drop-shadow(1px 1px 1px black' }}
+                >
+                  Skills
+                </h2>
+                <div className='w-full rounded-lg'>
+                  <Pictures user={user} user_images={user.overall_user_image} />
+                </div>
+              </section>
+
+              {/* Additional Skills or Content */}
+              <section>
+                <h2
+                  className='sticky top-0 z-10 pl-2 text-lg font-semibold text-white'
+                  style={{ filter: 'drop-shadow(1px 1px 1px black' }}
+                >
+                  Additional Skills
+                </h2>
+                <div className='w-full rounded-lg '>
+                  <Pictures user={user} user_images={user.overall_user_image} />
+                </div>
+              </section>
+            </div>
+          </div>
 
           {/* Guild */}
           <div className='flex flex-col pl-4 '>
--- src/components/ProfileComponent/PublicProfileComponent/ProfileInfoComponents/LeftSideComponents/Pictures.tsx
+++ src/components/ProfileComponent/PublicProfileComponent/ProfileInfoComponents/LeftSideComponents/Pictures.tsx
@@ -10,11 +10,18 @@ export default function Pictures({ user, user_images }) {
   return (
     <>
       <div className='w-full p-2'>
-        <Swiper slidesPerView={'auto'} spaceBetween={10} className='flex w-full items-center justify-center'>
+        <Swiper slidesPerView={3} spaceBetween={10} className='flex w-full items-center justify-center'>
           {user_images.map((image, i) => (
             <SwiperSlide key={i} style={{ width: 'auto' }}>
-              <div className='size-[100px] rounded-md'>
-                <Image src={image} alt='..' fill className='rounded-md object-cover' unoptimized quality={80} />
+              <div className='relative size-[110px] overflow-hidden rounded-md'>
+                <Image
+                  src={image}
+                  alt='..'
+                  fill
+                  className='rounded-md object-cover transition-all duration-300 ease-in-out hover:scale-125'
+                  unoptimized
+                  quality={80}
+                />
               </div>
             </SwiperSlide>
           ))}
