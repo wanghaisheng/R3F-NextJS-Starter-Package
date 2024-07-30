939d8f623d0799d7678098c03ba07821028917b7
RohitSth <shrestha.rohit655@gmail.com>
2024-07-30 15:43:11 +0545

    update :: tested animation for avatars

--- src/components/PublicProfileViews/LeftSideViewComponent.tsx
+++ src/components/PublicProfileViews/LeftSideViewComponent.tsx
@@ -1,7 +1,20 @@
+import CustomSwiper from '../MyComponents/CustomSwiper'
+
 export default function LeftSideViewComponent() {
   return (
     <div className='flex flex-col gap-2 p-2'>
       <div className='h-[200px] w-full rounded-lg bg-white/40'></div>
+
+      {/* <div className='p-4'>
+        <CustomSwiper>
+          <div className='size-[100px] bg-pink-300'></div>
+          <div className='size-[100px] bg-pink-300'></div>
+          <div className='size-[100px] bg-pink-300'></div>
+          <div className='size-[100px] bg-pink-300'></div>
+          <div className='size-[100px] bg-pink-300'></div>
+        </CustomSwiper>
+      </div> */}
+
       <div className='relative h-[305px] w-full overflow-auto'>
         <div className='mb-2 h-[100px] w-full rounded-lg bg-white/40'></div>
         <div className='my-2 h-[100px] w-full rounded-lg bg-white/40'></div>
